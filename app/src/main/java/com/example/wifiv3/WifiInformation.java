b3f621fd5e2760158b5d2e62ae5008f8e4e86974
Spectorn <lathom21@student.aau.dk>
2021-12-14 13:45:02 +0100

    v5

--- app/src/main/java/com/example/wifiv3/WifiInformation.java
+++ app/src/main/java/com/example/wifiv3/WifiInformation.java
@@ -26,23 +26,23 @@ public class WifiInformation {
         int Result;
         // 3mbps anses af mange for at være minimum til både streaming og gaming.
         if(Download < 3){
-            Score = -100;
+            Score = Score -100;
         }
         // 25mbps er hvad Netflix anbefaler for 4k streaming,
         else if(Download < 25){
-            Score = -60;
+            Score = Score -60;
         }
         // 8k streaming kræver 50 mbps
         else if(Download < 50){
-            Score = -30;
+            Score = Score -30;
         }
 
         // omkring 50 mbps anses af flere til at være tilstrækkeligt for en normal familie.
         else if (Download < 75) {
-            Score = -15;
+            Score = Score -15;
         }
         else if (Download < 75) {
-            Score = -5;
+            Score = Score -5;
         }
         System.out.println("Score efter første test" + Score);
         Result = DownloadToBasis(Score);
@@ -56,21 +56,20 @@ public class WifiInformation {
     }
 
     public int DownloadToBasis(int Score){
-        System.out.println("Score i anden test" + Score);
         long Diff = BasisDownload - Download;
         // hvis der er et fald på mere end 50% så er det et problem
         if (Diff > BasisDownload/2 ){
-            Score = -50;
+            Score = Score -50;
         }
         else if (Diff > BasisDownload/3){
-            Score = -30;
+            Score = Score -30;
         }
         else if (Diff > BasisDownload/4){
-            Score = -15;
+            Score = Score -15;
         }
         // Hvis testen bliver målt højere end basis er der ikke sket et fald og derfor må internettet være perfekt.
         else {
-            Score = +1;
+            Score = Score +1;
         }
 
         return  Score;
@@ -84,17 +83,17 @@ public class WifiInformation {
 
         /// Upload behøves kun at være en 1/10 af download til det meste brug
         if(Upload < 2){
-            Score = -80;
+            Score = Score -80;
         }
 
         else if (Upload < 5 && Upload > 2) {
-            Score = -40;
+            Score = Score -40;
         }
         else if (Upload < 10 && Upload > 5){
-            Score = -20;
+            Score = Score -20;
         }
         else if (Upload < 25 && Upload > 10){
-            Score = -10;
+            Score = Score -10;
         }
 
         Result = UploadToBasis(Score);
@@ -106,17 +105,17 @@ public class WifiInformation {
         long Diff = BasisUpload - Download;
         // hvis der er et fald på mere end 50% så er det et problem
         if (Diff > BasisUpload/2 ){
-            Score = -50;
+            Score = Score -50;
         }
         else if (Diff > BasisUpload/3){
-            Score = -30;
+            Score = Score -30;
         }
         else if (Diff > BasisUpload/4){
-            Score = -15;
+            Score = Score -15;
         }
         // Hvis testen bliver målt højere end basis er der ikke sket et fald og derfor må internettet være perfekt.
         else {
-            Score = +10;
+            Score = Score +1;
         }
         return  Score;
 
